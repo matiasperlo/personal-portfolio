29a95b4d91e500b08c22ba656f711873f462c56d
matiasperlo <103325934+matiasperlo@users.noreply.github.com>
2022-12-02 21:40:24 -0300

    validacion de privilegio de usuario solo en home, fraccionamiento en componentes
    - Componentes "edit-button" y "avatar-image" para derivar comportamientos y estilos más concretos.
    - Eliminación de Usuario y UsuarioService que no se usaban.
    - Nuevo icono de aplicacion.
    - Añadido logo Arg Programa en NavBar.
    - Actualización UserService usando pipe y tap del RxJs internamente para evitar ejecuciones multiples del observable.
    - Reubicación de estilos generales hacia el componente Home.

---
+++ src/app/home/avatar-image/avatar-image.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-avatar-image',
+  templateUrl: './avatar-image.component.html',
+  styleUrls: ['./avatar-image.component.css']
+})
+export class AvatarImageComponent implements OnInit {
+
+  @Input() urlImage: string;
+  @Input() urlEdit: string;
+  @Input() editable: boolean;
+
+  constructor() { 
+    this.urlImage = "";
+    this.urlEdit = "";
+    this.editable = false;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
